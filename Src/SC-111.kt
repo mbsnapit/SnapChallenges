7a9fa766e971fcc4717580e0da7d284714c83009
Chris Roberts <clroberts1138@gmail.com>
2018-12-18 12:01:38 -0600

    SC-110 -> Removed WhiteSpace and Comments SC-111 -> Removed WhiteSpace and Comments SC-112 -> Completed Code

--- Src/SC-111.kt
+++ Src/SC-111.kt
@@ -8,21 +8,34 @@
 //
 
 fun main(args: Array<String>) { //This is an inline comment
-
     var a: Int = 5
     var b: Int = 10
     var c: Int = b - a
-
     var callerResult = Array<Int>(c) { 0 }
-
-    callerResult = fizzArraay3(5,10)
-
+    callerResult = fizzArraay3(a, b)
     println("Parameters passed: $a, $b")
     var element: Int = 0
     for (e in callerResult) {
         print(" Array [$element] = $e, ")
         element++
-
+    }
+    a = 11
+    b = 18
+    println("")
+    callerResult = fizzArraay3(a, b)
+    println("Parameters passed: $a, $b")
+    for (e in callerResult) {
+        print(" Array [$element] = $e, ")
+        element++
+    }
+    a = 1
+    b = 3
+    println("")
+    callerResult = fizzArraay3(a, b)
+    println("Parameters passed: $a, $b")
+    for (e in callerResult) {
+        print(" Array [$element] = $e, ")
+        element++
     }
 }
 
@@ -30,10 +43,8 @@ fun fizzArraay3(start: Int, end: Int): Array<Int> {
     var n: Int = end - start;
     var newStart = start
     var result = Array<Int>(n) { 0 }
-
-    for (i in 0..n-1) {
+    for (i in 0..n - 1) {
         result[i] = newStart++
     }
-
     return result
 }
