04ba009736e14b234c99d68b574773092699e25f
Carlo <Carlo@snapit.solutions>
2018-12-18 13:18:33 -0600

    git ignore

--- src/ChrisRoberts/SC-110.kt
+++ src/ChrisRoberts/SC-110.kt
@@ -1,2 +1,77 @@
 package ChrisRoberts
 
+/**
+Given a non-negative int n, compute recursively (no loops) the count of the occurrences of 8 as a digit, except that an 8 with another 8 immediately to its left counts double, so 8818 yields 4. Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+Examples:
+ChrisRoberts.count8(8) → 1
+ChrisRoberts.count8(818) → 2
+ChrisRoberts.count8(8818) → 4
+ */
+fun main(args: Array<String>) { //This is an inline comment
+    var result = 0
+    var n = 8
+    result = count8(n)
+    println("The ChrisRoberts.count8 function passing $n resulted in $result")
+    n = 818
+    result = count8(n)
+    println("The ChrisRoberts.count8 function passing $n resulted in $result")
+    n = 8818
+    result = count8(n)
+    println("The ChrisRoberts.count8 function passing $n resulted in $result")
+    n = 8088
+    result = count8(n)
+    println("The ChrisRoberts.count8 function passing $n resulted in $result")
+    n = 123
+    result = count8(n)
+    println("The ChrisRoberts.count8 function passing $n resulted in $result")
+    n = 81238
+    result = count8(n)
+    println("The ChrisRoberts.count8 function passing $n resulted in $result")
+    n = 88788
+    result = count8(n)
+    println("The ChrisRoberts.count8 function passing $n resulted in $result")
+    n = 8234
+    result = count8(n)
+    println("The ChrisRoberts.count8 function passing $n resulted in $result")
+    n = 2348
+    result = count8(n)
+    println("The ChrisRoberts.count8 function passing $n resulted in $result")
+    n = 23884
+    result = count8(n)
+    println("The ChrisRoberts.count8 function passing $n resulted in $result")
+   n = 0
+    result = count8(n)
+    println("The ChrisRoberts.count8 function passing $n resulted in $result")
+    n = 1818188
+    result = count8(n)
+    println("The ChrisRoberts.count8 function passing $n resulted in $result")
+    n = 8818181
+    result = count8(n)
+    println("The ChrisRoberts.count8 function passing $n resulted in $result")
+    n = 1080
+    result = count8(n)
+    println("The ChrisRoberts.count8 function passing $n resulted in $result")
+    n = 188
+    result = count8(n)
+    println("The ChrisRoberts.count8 function passing $n resulted in $result")
+    n = 88888
+    result = count8(n)
+    println("The ChrisRoberts.count8 function passing $n resulted in $result")
+    n = 9898
+    result = count8(n)
+    println("The ChrisRoberts.count8 function passing $n resulted in $result")
+    n = 78
+    result = count8(n)
+    println("The ChrisRoberts.count8 function passing $n resulted in $result")
+}
+
+fun count8(n: Int, prevWas8: Boolean = false): Int {
+    var num8s: Int = 0;
+       if (n == 0) // base case
+        return 0
+    if (n % 10 == 8) //we found an 8!
+        num8s++
+    if (prevWas8 && num8s > 0) // we found two 8's in a row!
+        num8s++
+    return num8s + count8(n / 10, num8s > 0)
+}
--- src/ChrisRoberts/SC-111.kt
+++ src/ChrisRoberts/SC-111.kt
@@ -1,2 +1,52 @@
 package ChrisRoberts
 
+//
+//Given start and end numbers, return a new array containing the sequence of integers from start up to but not including end, so start=5 and end=10 yields {5, 6, 7, 8, 9}. The end number will be greater or equal to the start number. Note that a length-0 array is valid.
+//
+//Examples:
+//fizzArray3(5, 10) → [5, 6, 7, 8, 9]
+//fizzArray3(11, 18) → [11, 12, 13, 14, 15, 16, 17]
+//fizzArray3(1, 3) → [1, 2]
+//
+
+fun main(args: Array<String>) { //This is an inline comment
+    var a: Int = 5
+    var b: Int = 10
+    var c: Int = b - a
+    var callerResult = Array<Int>(c) { 0 }
+    callerResult = fizzArraay3(a, b)
+    println("Parameters passed: $a, $b")
+    var element: Int = 0
+    for (e in callerResult) {
+        print(" Array [$element] = $e, ")
+        element++
+    }
+    a = 11
+    b = 18
+    println("")
+    callerResult = fizzArraay3(a, b)
+    println("Parameters passed: $a, $b")
+    for (e in callerResult) {
+        print(" Array [$element] = $e, ")
+        element++
+    }
+    a = 1
+    b = 3
+    println("")
+    callerResult = fizzArraay3(a, b)
+    println("Parameters passed: $a, $b")
+    for (e in callerResult) {
+        print(" Array [$element] = $e, ")
+        element++
+    }
+}
+
+fun fizzArraay3(start: Int, end: Int): Array<Int> {
+    var n: Int = end - start;
+    var newStart = start
+    var result = Array<Int>(n) { 0 }
+    for (i in 0..n - 1) {
+        result[i] = newStart++
+    }
+    return result
+}
